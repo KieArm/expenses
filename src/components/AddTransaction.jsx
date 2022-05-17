7465e44c2d9c49c585d778b524bf3efc8e5df63d
KieArm <87212808+KieArm@users.noreply.github.com>
2022-05-17 10:51:10 +0800

    17/05/2022
    Cleared up error with build files uploaded.
    Added validation check to input.
    Added clear fields after new transaction.

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,38 +1,53 @@
-import React, { useState, useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
-
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
-    const [text, setText] = useState('');
-    const [amount, setAmount] = useState(0);
+  const [text, setText] = useState('')
+  const [amount, setAmount] = useState(0)
 
-    const { addTransaction } = useContext(GlobalContext);
+  const { addTransaction } = useContext(GlobalContext)
 
-    const onSubmit = e => {
-        e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault()
 
-        const newTransaction = {
-            id: Math.floor(Math.random() * 10000000),
-            text,
-            amount: +amount
-        }
-        addTransaction(newTransaction);
+    if (text === '' || amount === 0) {
+      return alert('Please enter a name and amount')
+    } else {
+      const newTransaction = {
+        id: Math.floor(Math.random() * 10000000),
+        text,
+        amount: +amount,
+      }
+      addTransaction(newTransaction)
+      setText('')
+      setAmount('')
     }
+  }
 
-    return (
-        <>
-            <h3>Add new transaction</h3>
-            <form onSubmit={onSubmit}>
-                <div className="form-control">
-                    <label htmlFor="text">Name:</label>
-                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="" />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="amount">Amount:</label>
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="" />
-                </div>
-                <button className="btn">Add transaction</button>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className='form-control'>
+          <label htmlFor='text'>Name:</label>
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder=''
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>Amount:</label>
+          <input
+            type='number'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder=''
+          />
+        </div>
+        <button className='btn'>Add transaction</button>
+      </form>
+    </>
+  )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export const Footer = () => {
-    return (
-        <a href="https://kierana.dev">
-            Return to kierana.dev
-        </a>
-    )
+  return (
+    <div className='footer'>
+      <p>Demo Updated: 17/5/2022</p>
+      <span>Return to: </span>
+      <a href='https://kierana.dev'>Kierana.dev</a>
+    </div>
+  )
 }
