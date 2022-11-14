3f0c745f197b6836f20bdca5d3b8cd88332d9f8c
KieArm <87212808+KieArm@users.noreply.github.com>
2022-11-14 19:35:50 +0000

    Update packages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
   return (
     <div className='footer'>
-      <p>Demo Updated: 17/5/2022</p>
+      <p>Demo Updated: 14/11/2022</p>
       <span>Return to: </span>
       <a href='https://kierana.dev'>Kierana.dev</a>
     </div>
-  )
-}
+  );
+};
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,31 +1,32 @@
-import React, { createContext, useReducer } from 'react'
-import AppReducer from './AppReducer'
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
 const initialState = {
   transactions: [
-    { id: 1, text: 'Salary', amount: 2500 },
-    { id: 2, text: 'Mortgage', amount: -1000 },
+    { id: 1, text: 'Salary', amount: 3000 },
+    { id: 2, text: 'Mortgage', amount: -1100 },
     { id: 3, text: 'Food', amount: -500 },
     { id: 4, text: 'Transport', amount: -150 },
+    { id: 5, text: 'Utilities', amount: -450 },
   ],
-}
+};
 
-export const GlobalContext = createContext(initialState)
+export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState)
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
   function deleteTransaction(id) {
     dispatch({
       type: 'DELETE_TRANSACTION',
       payload: id,
-    })
+    });
   }
   function addTransaction(transaction) {
     dispatch({
       type: 'ADD_TRANSACTION',
       payload: transaction,
-    })
+    });
   }
 
   return (
@@ -38,5 +39,5 @@ export const GlobalProvider = ({ children }) => {
     >
       {children}
     </GlobalContext.Provider>
-  )
-}
+  );
+};
